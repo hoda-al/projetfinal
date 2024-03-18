c24afa66ce448b807918e33de6d1c678b1b27868
hoda-al <161768571+hoda-al@users.noreply.github.com>
2024-03-18 16:27:08 +0200

    Add files via upload

---
+++ public/assets/for_company/script.js
@@ -0,0 +1,181 @@
+const slidePage = document.querySelector(".slide-page");
+const nextBtnFirst = document.querySelector(".firstNext");
+const prevBtnSec = document.querySelector(".prev-1");
+const nextBtnSec = document.querySelector(".next-1");
+const prevBtnThird = document.querySelector(".prev-2");
+const nextBtnThird = document.querySelector(".next-2");
+const prevBtnFourth = document.querySelector(".prev-3");
+const submitBtn = document.querySelector(".submit");
+const progressText = document.querySelectorAll(".step p");
+const progressCheck = document.querySelectorAll(".step .check");
+const bullet = document.querySelectorAll(".step .bullet");
+let current = 1;
+
+
+const form = document.querySelector("form"),
+  emailField = form.querySelector(".email-field"),
+  emailInput = emailField.querySelector(".email"),
+  passField = form.querySelector(".create-password"),
+  passInput = passField.querySelector(".password"),
+  cPassField = form.querySelector(".confirm-password"),
+  cPassInput = cPassField.querySelector(".cPassword");
+
+  var modal_wrapper = document.querySelector(".modal_wrapper");
+var shadow = document.querySelector(".shadow");
+
+
+  const dropArea = document.querySelector(".drop_box"),
+  button = dropArea.querySelector("button"),
+  dragText = dropArea.querySelector("header"),
+  input = dropArea.querySelector("input");
+let file;
+var filename;
+
+
+button.onclick = () => {
+  input.click();
+};
+input.addEventListener("change", function (e) {
+  var fileName = e.target.files[0].name;
+  let filedata = `
+    <form action="" method="post">
+    <div class="form">
+    <h4>${fileName}</h4>
+    <input type="email" placeholder="Enter email upload file">
+    <button class="btn">Upload</button>
+    </div>
+    </form>`;
+  dropArea.innerHTML = filedata;
+});
+
+
+
+
+// Email Validtion
+function checkEmail() {
+  const emaiPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+  if (!emailInput.value.match(emaiPattern)) {
+    return emailField.classList.add("invalid"); 
+  }
+  emailField.classList.remove("invalid"); 
+}
+
+// Hide and show password
+const eyeIcons = document.querySelectorAll(".show-hide");
+
+eyeIcons.forEach((eyeIcon) => {
+  eyeIcon.addEventListener("click", () => {
+    const pInput = eyeIcon.parentElement.querySelector("input"); 
+    if (pInput.type === "password") {
+      eyeIcon.classList.replace("bx-hide", "bx-show");
+      return (pInput.type = "text");
+    }
+    eyeIcon.classList.replace("bx-show", "bx-hide");
+    pInput.type = "password";
+  });
+});
+
+// Password Validation
+function createPass() {
+  const passPattern =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+  if (!passInput.value.match(passPattern)) {
+    return passField.classList.add("invalid"); 
+  }
+  passField.classList.remove("invalid"); 
+}
+
+// Confirm Password Validtion
+function confirmPass() {
+  if (passInput.value !== cPassInput.value || cPassInput.value === "") {
+    return cPassField.classList.add("invalid");
+  }
+  cPassField.classList.remove("invalid");
+}
+
+
+
+
+
+nextBtnFirst.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginRight = "-25%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+
+
+
+nextBtnSec.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginRight  = "-50%";
+  bullet[current - 1].classList.add("active");
+  progressCheck[current - 1].classList.add("active");
+  progressText[current - 1].classList.add("active");
+  current += 1;
+});
+
+
+nextBtnThird.addEventListener("click", function(event){
+
+
+// Calling Funtion on Form Sumbit
+
+event.preventDefault(); 
+checkEmail();
+createPass();
+confirmPass();
+
+//calling function on key up
+emailInput.addEventListener("keyup", checkEmail);
+passInput.addEventListener("keyup", createPass);
+cPassInput.addEventListener("keyup", confirmPass);
+
+if (
+  !emailField.classList.contains("invalid") &&
+  !passField.classList.contains("invalid") &&
+  !cPassField.classList.contains("invalid")
+) {
+
+  modal_wrapper.classList.add("active");}
+});
+
+
+shadow.addEventListener("click", function(){
+	modal_wrapper.classList.remove("active");
+})
+
+
+
+prevBtnSec.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginRight = "0%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+
+
+prevBtnThird.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginRight = "-25%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
+
+
+
+prevBtnFourth.addEventListener("click", function(event){
+  event.preventDefault();
+  slidePage.style.marginRight = "-50%";
+  bullet[current - 2].classList.remove("active");
+  progressCheck[current - 2].classList.remove("active");
+  progressText[current - 2].classList.remove("active");
+  current -= 1;
+});
